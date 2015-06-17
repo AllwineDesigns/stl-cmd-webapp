774c10ecc557b6ac8625fcdb4b1f16d8960d8eea
John Allwine <jallwine86@gmail.com>
2015-06-16 18:56:43 -0600

    oops

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/stl_threads', function(request, response) {
     var stl_threads = spawn("stl_threads", ["-D", "10", "-P", "2", "-h", "20", "/tmp/blah.stl"]);
-    stl_threads.on('close') function(code) {
+    stl_threads.on('close', function(code) {
         fs.readFile('/tmp/blah.stl', function(err,data) {
             response.send(data);
         });
