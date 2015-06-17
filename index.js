0431bbf62006193346b8b306354b611a5f5dffa9
John Allwine <jallwine86@gmail.com>
2015-06-16 18:54:31 -0600

    stuff

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+var spawn = require('child_process').spawn;
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/stl_threads', function(request, response) {
-  response.send('Hello World!');
+    var stl_threads = spawn("stl_threads", ["-D", "10", "-P", "2", "-h", "20", "/tmp/blah.stl"]);
+    stl_threads.on('close') function(code) {
+        fs.readFile('/tmp/blah.stl', function(err,data) {
+            response.send(data);
+        });
+    });
 });
 
 app.listen(app.get('port'), function() {
