4ac02ade3c1fca9a55da6e591dc2ff098869421d
John Allwine <jallwine86@gmail.com>
2015-06-16 22:34:18 -0600

    added command line arguments as GET parameters

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/stl_threads', function(request, response) {
     var file = "/tmp/" + uuid.v4() + ".stl";
-    var stl_threads = spawn("stl_threads", ["-D", "10", "-P", "2", "-h", "20", file]);
+
+    var f = request.query.f ? true : false;
+    var D = request.query.D ? request.query.D : "6.35";
+    var P = request.query.P ? request.query.P : "1.27";
+    var h = request.query.h ? request.query.h : "25.4";
+    var a = request.query.a ? request.query.a : "60";
+    var s = request.query.s ? request.query.s : "72";
+    var o = request.query.o ? request.query.o : "12";
+
+    var stl_threads = spawn("stl_threads", ["-D", D, "-P", P, "-h", h, "-a", a, "-s", s, "-o", o, file]);
     stl_threads.on('close', function(code) {
         response.download(file, 'stl_threads.stl');
     });
