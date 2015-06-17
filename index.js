545091f04084098fec5e48d7d5afedf21cb5713b
John Allwine <jallwine86@gmail.com>
2015-06-16 22:22:35 -0600

    using response.download

--- index.js
+++ index.js
@@ -2,22 +2,16 @@ var express = require('express');
 var app = express();
 var spawn = require('child_process').spawn;
 var fs = require('fs');
+var uuid = require('node-uuid');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/stl_threads', function(request, response) {
-    var stl_threads = spawn("stl_threads", ["-D", "10", "-P", "2", "-h", "20", "/tmp/blah.stl"]);
+    var file = "/tmp/" + uuid.v4() + ".stl";
+    var stl_threads = spawn("stl_threads", ["-D", "10", "-P", "2", "-h", "20", file]);
     stl_threads.on('close', function(code) {
-        fs.readFile('/tmp/blah.stl', function(err,data) {
-            if(err) {
-                response.writeHead(500);
-                response.end();
-            } else {
-                response.writeHead(200, { 'Content-Type': 'application/sla' });
-                response.end(data);
-            }
-        });
+        response.download(file, 'stl_threads.stl');
     });
 });
 
