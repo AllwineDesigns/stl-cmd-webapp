17b6fd1b18003ab944b85bc370e5739dd7f71569
John Allwine <jallwine86@gmail.com>
2015-06-17 13:06:51 -0600

    added stl_threads_english for imperial measurements (still outputs stl in mm)

--- index.js
+++ index.js
@@ -29,6 +29,28 @@ app.get('/stl_threads', function(request, response) {
     });
 });
 
+app.get('/stl_threads_english', function(request, response) {
+    var file = "/tmp/" + uuid.v4() + ".stl";
+
+    var D = request.query.D ? parseFloat(request.query.D)*25.4 : "6.35";
+    var P = request.query.tpi ? 25.4/parseFloat(request.query.tpi) : "1.27";
+    var h = request.query.h ? parseFloat(request.query.h)*25.4 : "25.4";
+    var a = request.query.a ? request.query.a : "60";
+    var s = request.query.s ? request.query.s : "72";
+    var o = request.query.o ? parseFloat(request.query.o)*25.4 : "12";
+
+    var args = ["-D", D, "-P", P, "-h", h, "-a", a, "-s", s, "-o", o];
+    if(request.query.f) {
+        args.push("-f");
+    }
+    args.push(file);
+
+    var stl_threads = spawn("stl_threads", args);
+    stl_threads.on('close', function(code) {
+        response.download(file, 'stl_threads.stl');
+    });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
